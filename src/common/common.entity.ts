15eaae4b90e59bc7694e0a849389227a3188b16a
santa <sawntamagar36@gmail.com>
2023-06-13 15:30:21 +0545

    updated models of user  with base user models

---
+++ src/common/common.entity.ts
@@ -0,0 +1,12 @@
+import { PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
